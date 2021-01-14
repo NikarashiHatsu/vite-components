b4537f01393aed65d0687318e6c1486dc90b5eb4
NikarashiHatsu <yourlovelydev@gmail.com>
2021-01-14 23:07:31 +0700

    add: tailwind line-clamp

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/aspect-ratio')
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp')
   ],
 }
